9bd00b6514692078c6959cb570df88f96bd6a1a2
dishanayak383 <dishanayak383@gmail.com>
2025-03-11 15:34:30 +0530

    first commit

--- my-app/src/Components/Hall.jsx
+++ my-app/src/Components/Hall.jsx
@@ -3,7 +3,15 @@ import "./Hall.css";
 
 const Hall =()=> {
     return (
-        <div className="container"></div>
+        <div className="container">
+            <div className="box1">
+                <div className="image">
+                    <img src="https://png.pngtree.com/png-vector/20240822/ourmid/pngtree-beach-logo-design-inspiration-sunset-coconut-trees-palms-on-the-vector-png-image_13564239.png"/>
+                </div>
+            </div>
+            <div className="box2"></div>
+          
+        </div>
     );
 };
 
