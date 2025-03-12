f5c6143a4a8a0cd74da5d559c07c09cce57b5fd7
dishanayak383 <dishanayak383@gmail.com>
2025-03-12 10:16:07 +0530

    first commit

--- my-app/src/Components/Hall.jsx
+++ my-app/src/Components/Hall.jsx
@@ -7,6 +7,8 @@ const Hall =()=> {
             <div className="box1">
                 <div className="image">
                     <img src="https://png.pngtree.com/png-vector/20240822/ourmid/pngtree-beach-logo-design-inspiration-sunset-coconut-trees-palms-on-the-vector-png-image_13564239.png"/>
+                    <div className="kumta">Namma Kumata</div>
+                    <div className="notification"></div>
                 </div>
             </div>
             <div className="box2"></div>
